2243f394d367dc8acc26a1800829a388b383ffd5
alimuamar11 <33304709+alimuamar11@users.noreply.github.com>
2017-12-27 16:35:03 +0700

    Add files via upload

---
+++ index/index.php
@@ -0,0 +1,8 @@
+<?php
+echo "<b>";
+echo "<h2>Tugas Teori TCC</h2>";
+echo "<br>";
+echo "<h3>Nama 		: Ali Muamar Jatmiko<br>";
+echo "NIM 		: 155410002<br>";
+echo "Jurusan 	: Teknik Informatika</h3>";
+?>
